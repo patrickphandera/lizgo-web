43b5cd1a157407da7460149bdfa4b9d6c749c590
patrickphandera <patrickphandera@gmail.com>
2026-01-08 04:16:36 +0200

    improved lessons page

--- src/components/LessonFirstCard.vue
+++ src/components/LessonFirstCard.vue
@@ -1,49 +1,44 @@
 <template>
   <div class="col-12 col-sm-6 col-md-4">
-    <q-card  clickable class="q-my-sm shadow-1">
-      <router-link :to="`/students/lessons/${lesson?.id}`" style="text-decoration: none; color: inherit;">
+    <q-card clickable class="q-my-sm shadow-1">
+      <router-link
+        :to="`/students/lessons/${lesson?.id}`"
+        style="text-decoration: none; color: inherit"
+      >
         <q-img :src="lesson?.thumbnail" height="140px">
-          <div class="absolute-full text-subtitle2 flex flex-center">
-          </div>
+          <div class="absolute-full text-subtitle2 flex flex-center"></div>
         </q-img>
       </router-link>
 
       <div>
+        <div class="row items-center flex">
+          <div class="text-body1 text-weight-medium ellipsis q-px-sm">
+            {{ lesson?.title }}
+          </div>
+        </div>
+        <div class="text-caption flex q-px-xs">
+          <q-chip square class="text-caption" dense>{{ lesson?.curriculumId?.title }}</q-chip>
+          <q-chip square class="text-caption" dense>{{ lesson?.subjectId?.title }} </q-chip>
+          <q-chip square class="text-caption" dense v-if="lesson?.book">{{ lesson?.book }} </q-chip>
+          <q-chip square class="text-caption" dense>{{ lesson?.topicId.title }}</q-chip>
+          <!-- <q-chip square class="text-caption " dense>30K Views</q-chip> -->
 
-          <div class="row items-center flex">
-
-            <div class="text-body1 text-weight-medium ellipsis q-px-sm">
-              {{
-                lesson?.title }}
+          <q-chip square class="text-caption" dense> {{ lesson?.timeAgo }}</q-chip>
+        </div>
+        <div class="flex q-my-xs row q-pa-sm">
+          <div class="row q-gutter-sm">
+            <q-avatar size="40px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+            <div class="column">
+              <div square class="text-caption" dense>Patrick Phandera</div>
+              <div square class="text-caption text-grey-8" dense>30k Followers</div>
+              <q-space />
             </div>
           </div>
-          <div class="text-caption flex q-px-xs">
-            <q-chip square class="text-caption " dense>{{
-              lesson?.curriculumId?.title }}</q-chip>
-            <q-chip square class="text-caption" dense>{{
-              lesson?.subjectId?.title }} </q-chip>
-            <q-chip square class="text-caption" dense v-if="lesson?.book">{{
-              lesson?.book }} </q-chip>
-            <q-chip square class="text-caption " dense>{{
-              lesson?.topicId.title }}</q-chip>
-            <!-- <q-chip square class="text-caption " dense>30K Views</q-chip> -->
-
-            <q-chip square class="text-caption " dense>
-              {{ lesson?.timeAgo }}</q-chip>
-          </div>
-          <div class="flex q-my-xs row q-pa-sm">
-            <div class="row q-gutter-sm">
-              <q-avatar size="40px">
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-              </q-avatar>
-              <div class="column">
-                <div square class="text-caption " dense>Patrick Phandera</div>
-                <div square class="text-caption text-grey-8" dense>30k Followers</div> <q-space />
-              </div>
-            </div>
-            <q-space></q-space>
-            <q-btn flat round dense icon="more_vert" />
-          </div>
+          <q-space></q-space>
+          <q-btn flat round dense icon="more_vert" />
+        </div>
       </div>
     </q-card>
   </div>
@@ -57,8 +52,9 @@ export default {
   setup() {
     return {
       expanded: ref(false),
-      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      lorem:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     }
-  }
+  },
 }
 </script>
